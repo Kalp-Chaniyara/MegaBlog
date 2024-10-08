ae2710cde88125399e689dbd4f8f1a25fa75f80b
Kalp-Chaniyara <kalpchaniyara.2006@gmail.com>
2024-10-08 20:51:55 +0530

    setting up the env

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_PROJECT_URL),
+    appwriteDatabaseId : String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_BUCKET_ID),
+}
+
+export default config
+
+//! just a production gred approach to ensure that all value we access must in string
