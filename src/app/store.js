7820e22273be2e387c5a4cfdbfd3d0a974f7506a
Kalp-Chaniyara <kalpchaniyara.2006@gmail.com>
2024-10-10 02:53:22 +0530

    setting up the store(Redux ToolKit)

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit"
+
+const store = configureStore({
+    reducer:{
+        //here we can add multiple reducers
+    }
+});
+
+export default store
--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -16,6 +16,8 @@ export class Services{
         this.bucket = new Storage(this.client);
     }
 
+    //Post services
+
     async createPost({title,slug,content,featuredImamge,status,userId}){
         try {
             return await this.databases.createDocument(
@@ -86,6 +88,54 @@ export class Services{
             console.log("Appwrite :: getPost error :: ", error);
             return false;
         }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch(error){
+            console.log("Appwrite :: getPosts error :: ", error);
+            return false;
+        }
+    }
+
+    //File Upload services
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch(error){
+            console.log("Appwrite :: uploadFile error :: ", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId //When we create the file we will get file id which we generate through ID.unique() and store this into the featuredimage
+            )
+            return true;
+        } catch(error){
+            console.log("Appwrite :: deleteFile error :: ", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){ // No need to use async-awiat bcz its result is so fast and    doesn't return promise as per documentation
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
